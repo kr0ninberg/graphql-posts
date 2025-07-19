08121e6e9cfe0d2cb65e59c6e2679fe984def59d
Maksim Shahov <utypostev@gmail.com>
2025-07-19 14:38:48 +0300

    basic posts

---
+++ graph/resolver.go
@@ -0,0 +1,10 @@
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+package graph
+
+import "graphql-ozon/graph/model"
+
+type Resolver struct {
+	Posts []*model.Post
+}
